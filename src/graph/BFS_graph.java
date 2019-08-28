9cc9891a167a0ffe9547ed21d079c3c68cb6f2fb
KAILASH KUMAR PRAJAPATI <kailash12manit@gmail.com>
2019-08-28 23:39:37 +0530

    updated

--- src/graph/BFS_graph.java
+++ src/graph/BFS_graph.java
@@ -1,65 +1,55 @@
-package graph;
+package Graph;
+import java.util.*;
+public class BFS_Graph {
+	private int V;
+	private LinkedList<Integer> adj[];
 
-import java.util.Iterator;
-import java.util.LinkedList;
-
-public class BFS_graph {
-
-	int V;
-	LinkedList<Integer> []adjLinkedlist;
-	public BFS_graph(int v) {
-		this.V=v;
-		adjLinkedlist = new LinkedList[v];
-		for(int i=0;i<v;i++) {
-			adjLinkedlist[i]= new LinkedList<Integer>();
-		}
+	public BFS_Graph(int v) {
+		// TODO Auto-generated constructor stub
+		V=v;
+		adj= new LinkedList[v];
+		for(int i=0;i<v;i++){
+			adj[i]= new LinkedList<Integer>();
+		}		
 	}
-	
-	public void addEdge(int src, int dst) {
-		// TODO Auto-generated method stub
-		adjLinkedlist[src].add(dst);
-
+	void addEdge(int a,int b){
+		adj[a].add(b);
 	}
-	public void BFS(int start) {
+
+	private void BFS(int s) {
 		// TODO Auto-generated method stub
 		boolean visited[]= new boolean[V];
-		
 		LinkedList<Integer> queue = new LinkedList<Integer>();
-
-		visited[start]=true;
-		queue.add(start);
-		
-		while(queue.size()!=0) {
-			start=queue.poll();
-			System.out.print(start+" ");
-			
-			Iterator<Integer> itr = adjLinkedlist[start].listIterator();
-			while(itr.hasNext()) {
-				int n = itr.next();
-				
-				if(!visited[n]) {
+		visited[s]=true;
+		queue.add(s);
+		while(queue.size()!=0){
+			s=queue.poll();
+			System.out.print(s+" ");
+			Iterator<Integer> i = adj[s].listIterator();
+			while(i.hasNext()){
+				int n=i.next();
+				if(!visited[n]){
 					visited[n]=true;
 					queue.add(n);
-				}						
+				}
 			}
-		}		
+		}
 	}
 
 	public static void main(String[] args) {
+		BFS_Graph g = new BFS_Graph(4);
 
-		BFS_graph g = new BFS_graph(4); 
+		g.addEdge(0, 1);
+		g.addEdge(0, 2);
+		g.addEdge(1, 2);
+		g.addEdge(2, 0);
+		g.addEdge(2, 3);
+		g.addEdge(3, 3);
 
-		 g.addEdge(0, 1); 
-	        g.addEdge(0, 2); 
-	        g.addEdge(1, 2); 
-	        g.addEdge(2, 0); 
-	        g.addEdge(2, 3); 
-	        g.addEdge(3, 3); 
-	  
-	        System.out.println("Following is Breadth First Traversal "+ 
-	                           "(starting from vertex 2)"); 
-	  
-	        g.BFS(2); 
+		System.out.println("Following is Breadth First Traversal "+
+				"(starting from vertex 2)");
 
-	}
+		g.BFS(2);
+
+	}	
 }
